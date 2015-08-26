e74c3a9f7513788825a3d0d626579c2a9f54b9cc
Timekiller11 <charlesforest11@gmail.com>
2015-08-26 01:08:29 -0400

    Cleaned lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -6,53 +6,68 @@ fi
 # Variables.
 HOSTNAME="cserver.me"
 
-
-# Create update script for cron job
+echo "Create directories"
+echo "cron job"
 mkdir -p /root/cronjobs
-
-
-echo "Dependencies and desired softwares"
-
-aptitude install -y build-essential &> /dev/null
-aptitude install -y python-software-properties &> /dev/null
-aptitude install -y libdate-manip-perl &> /dev/null
-
-
-
-. modules/composer.sh &> /dev/null
-. modules/nmap.sh &> /dev/null
-. modules/git.sh &> /dev/null
-. modules/curl.sh &> /dev/null
-
-
-# Create website directory
+echo "websites"
 mkdir -p /var/www
 
 
-. modules/mariadb.sh &> /dev/null
-. modules/nginx.sh &> /dev/null
-. modules/php.sh &> /dev/null
+echo "Dependencies"
+echo "build-essential"
+aptitude install -y build-essential >/dev/null 2>&1
+echo "python-software-properties"
+aptitude install -y python-software-properties >/dev/null 2>&1
+echo "libdate-manip-perl"
+aptitude install -y libdate-manip-perl >/dev/null 2>&1
+
+echo "Securing the server"
+echo "hots"
+. modules/secure_host.sh >/dev/null 2>&1
+echo "shared"
+. modules/secure_shared.sh >/dev/null 2>&1
+echo "syctl"
+. modules/secure_sysctl.sh >/dev/null 2>&1
 
 
+echo "Installing softwares"
+echo "nmap"
+. modules/nmap.sh >/dev/null 2>&1
+echo "git"
+. modules/git.sh >/dev/null 2>&1
+echo "curl"
+. modules/curl.sh >/dev/null 2>&1
+echo "php"
+. modules/php.sh >/dev/null 2>&1
+echo "mariadb"
+. modules/mariadb.sh >/dev/null 2>&1
+echo "nginx"
+. modules/nginx.sh >/dev/null 2>&1
+echo "composer"
+. modules/composer.sh >/dev/null 2>&1
 
-# Securing the server
-. modules/secure_host.sh &> /dev/null
-. modules/secure_shared.sh &> /dev/null
-. modules/secure_sysctl.sh &> /dev/null
 
-# Installing security appliances
-. modules/apparmor.sh &> /dev/null
-. modules/chkrootkit.sh &> /dev/null
-. modules/fail2ban.sh &> /dev/null
-. modules/iwatch.sh &> /dev/null
-. modules/logwatch.sh &> /dev/null
-. modules/rkhunter.sh &> /dev/null
-. modules/tiger.sh &> /dev/null
+echo "Installing security appliances"
+echo "apparmor"
+. modules/apparmor.sh >/dev/null 2>&1
+echo "chkrootkit"
+. modules/chkrootkit.sh >/dev/null 2>&1
+echo "fail2ban"
+. modules/fail2ban.sh >/dev/null 2>&1
+#echo ""
+#. modules/iwatch.sh >/dev/null 2>&1
+echo "logwatch"
+. modules/logwatch.sh >/dev/null 2>&1
+echo "rkhunter"
+. modules/rkhunter.sh >/dev/null 2>&1
+echo "tiger"
+. modules/tiger.sh >/dev/null 2>&1
 
-# Installing Firewall
-. modules/ufw.sh &> /dev/null
-
-. modules/psad.sh &> /dev/null
+echo "Installing Firewall"
+echo "UFW"
+. modules/ufw.sh >/dev/null 2>&1
+echo "psad"
+. modules/psad.sh >/dev/null 2>&1
 
 
 
